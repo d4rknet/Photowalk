d5aea2717e72b428fe875a01884c8baa5bdd44f9
NicklasLilie <31849930+NicklasLilie@users.noreply.github.com>
2017-10-03 16:05:49 +0200

    Tested the Websocket

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,53 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('localhost:8080/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/chat/nicklas', function (greeting) {
+           // showGreeting(JSON.parse(greeting.body).content);
+            console.log (greeting);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    var chatMessage = {sender : "alex" , reciever: "nicklas", message: "Hallo es klappt"}
+    stompClient.send("/app/sendMessage", {}, JSON.stringify(chatMessage));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
